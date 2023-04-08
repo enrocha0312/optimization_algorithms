c3e94bcf9c827cdc694ebc66d1aebe07e8e9a838
enrocha0312 <enrocha0312@gmail.com>
2023-04-08 11:11:16 -0300

    initialization for continuous genetic algorithm

---
+++ Continuous_Genetic_Algorithm.py
@@ -0,0 +1,44 @@
+import random as rd
+import math
+import sys
+import numpy as np
+
+
+def fitnessFuction(population):
+    return list(map(lambda x: x*x, population))
+    # you can modify to whatever function you want, here it's just an example
+    # it calculates the square for any number
+
+class Chromosome:
+    def __init__(self, genes):
+        self.genes = genes
+    def calculateFitnessValues(genes, callback):
+        return list(map(callback,genes))
+
+def LimitsCalculate(numberOfGenes):
+    upperLimit = np.ones(numberOfGenes, dtype=int) * 100
+    lowerLimit = np.ones(numberOfGenes, dtype=int) * (-100)
+    return [upperLimit, lowerLimit]
+#chosen just for this case
+#this is the space for your solution, the limits of the values of your Chromosome's genes
+
+def visualizePopulation(population):
+    for i in range(len(population)):
+        print("Chromosome " , i)
+        print(population[i].genes)
+
+def populationInitializate(numberOfChromosomes, numberOfGenes, upperLimits, lowerLimits):
+    population = []
+    for i in range(numberOfChromosomes):
+        population.append(Chromosome([]))
+    for i in range(numberOfChromosomes):
+        for j in range(numberOfGenes):
+            population[i].genes.append((upperLimits[j] - lowerLimits[j]) * rd.random() + lowerLimits[j])
+    return population
+
+
+upperLimit, lowerLimit = LimitsCalculate(10)
+
+population = populationInitializate(2, 10, upperLimit, lowerLimit)
+
+visualizePopulation(population)
