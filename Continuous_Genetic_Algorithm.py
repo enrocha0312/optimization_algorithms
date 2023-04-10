abe069129036e4fb2d83450f44c21592765c33c8
enrocha0312 <enrocha0312@gmail.com>
2023-04-10 11:27:50 -0300

    Roulette wheel selection

--- Continuous_Genetic_Algorithm.py
+++ Continuous_Genetic_Algorithm.py
@@ -15,10 +15,9 @@ class Chromosome:
     def calculateFitnessValueOfTheChromosome(self, callback):
         return sum(map(callback, self.genes))
 
-
-def LimitsCalculate(numberOfGenes):
-    upperLimit = np.ones(numberOfGenes, dtype=int) * 100
-    lowerLimit = np.ones(numberOfGenes, dtype=int) * (-100)
+def LimitsCalculate(numberOfGenes, up=100, lp=-100):
+    upperLimit = np.ones(numberOfGenes, dtype=int) * up
+    lowerLimit = np.ones(numberOfGenes, dtype=int) * lp
     return [upperLimit, lowerLimit]
 #chosen just for this case
 #this is the space for your solution, the limits of the values of your Chromosome's genes
@@ -28,7 +27,7 @@ def visualizePopulation(population):
         print("Chromosome " , i)
         print(population[i].genes)
 
-def populationInitializate(numberOfChromosomes, numberOfGenes, upperLimits, lowerLimits):
+def populationInitialize(numberOfChromosomes, numberOfGenes, upperLimits, lowerLimits):
     population = []
     for i in range(numberOfChromosomes):
         population.append(Chromosome([]))
@@ -37,9 +36,40 @@ def populationInitializate(numberOfChromosomes, numberOfGenes, upperLimits, lowe
             population[i].genes.append((upperLimits[j] - lowerLimits[j]) * rd.random() + lowerLimits[j])
     return population
 
+def rouletteWheelSelection(population):
+    populationOrderedByFitness = sorted(population,
+                                        key=lambda p: p.calculateFitnessValueOfTheChromosome(fitnessFuction),
+                                        reverse=True)
+    print("Chromosomes Adresses ordered by their fitness:")
+    print(populationOrderedByFitness)
+    fitnessList = list(map(lambda p: p.calculateFitnessValueOfTheChromosome(fitnessFuction), population))
+    cumSum = list(np.cumsum([c/sum(fitnessList) for c in fitnessList]))
+    cumSum.sort(reverse=True)
+    print("\n Cumulative sum in descending order:" )
+    print(cumSum)
+
+    R  =  rd.random()
+    parent1Index = len(cumSum) - 1 #guarantee that it has an index if we can't be in the if condition
+    for i in range(len(cumSum)):
+        if R > cumSum[i]:
+            parent1Index = i-1
+            break
+
+    parent2Index = parent1Index
+    while parent2Index == parent1Index:
+        R = rd.random()
+        for i in range(len(cumSum)):
+            if R>cumSum[i]:
+                parent2Index = i - 1;
+                break
+    parent1 = populationOrderedByFitness[parent1Index]
+    parent2 = populationOrderedByFitness[parent2Index]
+    return [parent1, parent2]
 
 upperLimit, lowerLimit = LimitsCalculate(10)
 
-population = populationInitializate(2, 10, upperLimit, lowerLimit)
+population = populationInitialize(5, 10, upperLimit, lowerLimit)
 
 visualizePopulation(population)
+
+print(rouletteWheelSelection(population))
