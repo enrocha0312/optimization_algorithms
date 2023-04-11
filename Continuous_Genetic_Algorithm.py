49c069e0ee51a0fc17a1cd845b832ea891363ec4
enrocha0312 <enrocha0312@gmail.com>
2023-04-10 23:25:34 -0300

    Crossover function

--- Continuous_Genetic_Algorithm.py
+++ Continuous_Genetic_Algorithm.py
@@ -47,14 +47,12 @@ def rouletteWheelSelection(population):
     cumSum.sort(reverse=True)
     print("\n Cumulative sum in descending order:" )
     print(cumSum)
-
     R  =  rd.random()
     parent1Index = len(cumSum) - 1 #guarantee that it has an index if we can't be in the if condition
     for i in range(len(cumSum)):
         if R > cumSum[i]:
             parent1Index = i-1
             break
-
     parent2Index = parent1Index
     while parent2Index == parent1Index:
         R = rd.random()
@@ -66,10 +64,76 @@ def rouletteWheelSelection(population):
     parent2 = populationOrderedByFitness[parent2Index]
     return [parent1, parent2]
 
+def crossover(parent1, parent2, crossoverProbability, crossoverType):
+    match crossoverType:
+        case "single":
+            ub = len(parent1.genes) - 1
+            lb = 0
+            Cross_P = round ((ub-lb) * rd.random() + lb)
+            #We have to be sure that cross_P is not 0 or the upper limit
+            while(Cross_P == 0 or Cross_P == ub ):
+                Cross_P = round((ub-lb) * rd.random() + lb)
+            part1FromChild1 = parent1.genes[0:Cross_P]
+            part2FromChild1 = parent2.genes[Cross_P:]
+            child1 = Chromosome(part1FromChild1+part2FromChild1)
+            part1FromChild2 = parent2.genes[0:Cross_P]
+            part2FromChild2 = parent1.genes[Cross_P:]
+            child2 = Chromosome(part1FromChild2+part2FromChild2)
+        case 'double':
+            ub = len(parent1.genes) - 1
+            lb = 0
+            Cross_P1 = round((ub - lb) * rd.random() + lb)
+            while(Cross_P1 == 0 or Cross_P1 == ub ):
+                Cross_P1 = round((ub-lb) * rd.random() + lb)
+            Cross_P2 = Cross_P1
+            while(Cross_P2 == Cross_P1 or Cross_P2 == 0 or Cross_P2 == ub):
+                Cross_P2 = round((ub - lb) * rd.random() + lb)
+            if Cross_P1 > Cross_P2:
+                [Cross_P1, Cross_P2] = [Cross_P2,Cross_P1]
+            part1FromChild1 = parent1.genes[0:Cross_P1]
+            part2FromChild1 = parent2.genes[Cross_P1:Cross_P2]
+            part3FromChild1 = parent1.genes[Cross_P2:]
+            child1 = Chromosome(part1FromChild1 + part2FromChild1 + part3FromChild1)
+            part1FromChild2 = parent2.genes[0:Cross_P1]
+            part2FromChild2 = parent1.genes[Cross_P1:Cross_P2]
+            part3FromChild2 = parent2.genes[Cross_P2:]
+            child2 = Chromosome(part1FromChild2 + part2FromChild2 + part3FromChild2)
+    R1 = rd.random()
+    if R1<=crossoverProbability:
+        child1 = child1
+    else:
+        child1 = parent1
+    R2 = rd.random()
+    if R2<=crossoverProbability:
+        child2 = child2
+    else:
+        child2 = parent2
+    return [child1, child2]
+
+
+
+
+
+
+
+
+
 upperLimit, lowerLimit = LimitsCalculate(10)
 
 population = populationInitialize(5, 10, upperLimit, lowerLimit)
 
 visualizePopulation(population)
 
-print(rouletteWheelSelection(population))
+[parent1, parent2] = rouletteWheelSelection(population)
+print("PARENT 1")
+print(parent1.genes)
+print("PARENT 2")
+print(parent2.genes)
+
+
+[child1, child2] = crossover(parent1, parent2, 0.85, "double")
+
+print("CHILD 1")
+print(child1.genes)
+print("CHILD 2")
+print(child2.genes)
